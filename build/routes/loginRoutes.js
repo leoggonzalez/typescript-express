077c9bbb35d15a2ad9238d6b5483746de9e665d3
leoggonzalez <leoggonzalez@hotmail.com>
2020-02-18 09:21:00 +0100

    added login form

--- build/routes/loginRoutes.js
+++ build/routes/loginRoutes.js
@@ -3,6 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = require("express");
 var router = express_1.Router();
 exports.router = router;
-router.get("/", function (req, res) {
-    res.send("\n    <div>\n      <h1>Hi there \uD83D\uDE42</h1>\n    </div>\n  ");
+router.get('/login', function (req, res) {
+    res.send("\n    <Form method=\"POST\">\n      <div>\n        <label>Email</label>\n        <input name=\"test\" />\n      </div>\n      <div>\n        <label>Password</label>\n        <input name=\"password\" type=\"password\" />\n      </div>\n      <button>Submit</button>\n    </form>\n  ");
+});
+router.post('/login', function (req, res) {
+    var _a = req.body, email = _a.email, password = _a.password;
+    res.send(email.toUpperCase());
 });
--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,13 +1,27 @@
-import { Router, Request, Response } from "express";
+import { Router, Request, Response } from 'express';
 
 const router = Router();
 
-router.get("/", (req: Request, res: Response) => {
+router.get('/login', (req: Request, res: Response) => {
   res.send(`
-    <div>
-      <h1>Hi there 🙂</h1>
-    </div>
+    <Form method="POST">
+      <div>
+        <label>Email</label>
+        <input name="test" />
+      </div>
+      <div>
+        <label>Password</label>
+        <input name="password" type="password" />
+      </div>
+      <button>Submit</button>
+    </form>
   `);
+});
+
+router.post('/login', (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  res.send(email.toUpperCase());
 });
 
 export { router };
